8ca40444c820de619abd1a02fb57367a05d97c3a
Tomáš Korenko <radixxko@gmail.com>
2026-07-22 09:09:42 +0200

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
 
-# Install dependencies and build library
+# Install dependencies, Jazzer.js core fuzzer engine, and build library
 npm ci
+npm install --no-save @jazzer.js/core
 npm run build
 
 # Compile Jazzer.js fuzz target into $OUT directory
 compile_javascript_fuzzer object-hash .clusterfuzzlite/fuzz_hash.js
+
