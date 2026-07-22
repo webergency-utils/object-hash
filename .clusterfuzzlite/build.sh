a6aa3bf2835868ae9796bdfd20c345aa1f1cb74d
Tomáš Korenko <radixxko@gmail.com>
2026-07-22 09:19:53 +0200

    fix: rename fuzz target to .cjs and add @jazzer.js/core to package.json

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -6,5 +6,6 @@ npm install --no-save @jazzer.js/core
 npm run build
 
 # Compile Jazzer.js fuzz target into $OUT directory
-compile_javascript_fuzzer object-hash .clusterfuzzlite/fuzz_hash.js
+compile_javascript_fuzzer object-hash .clusterfuzzlite/fuzz_hash.cjs
+
 
